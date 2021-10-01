2b98a7206091556a2ae3ef7a01987e8bf730a9ff
babud <babud@juniper.net>
2021-10-01 07:22:04 +0000

    rpc_execute

---
+++ onbox_python/op_script/rpc_execute.py
@@ -0,0 +1,6 @@
+from jnpr.junos import Device
+from lxml import etree
+
+with Device() as jdev:
+    rsp = jdev.rpc.get_interface_information(interface_name='fxp0', terse=True)
+    print (etree.tostring(rsp, encoding='unicode'))
